0afcf0e3532b357d8596a7b1f94375adf49b0781
Jane <waguthijane61@gmail.com>
2024-07-12 00:41:44 -0700

    auth

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,66 @@
+
+#!/usr/bin/env python3
+"""Handle Basic Authentication
+for Users
+"""
+from flask import request
+from fnmatch import fnmatch
+from typing import List, TypeVar
+from os import getenv
+
+
+class Auth:
+    """Auth Class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """requires authentication
+
+        Args:
+            path (str): url
+            excluded_paths (List[str]): excluded url
+
+        Returns:
+            bool: returns False or True
+        """
+        if path is not None and excluded_paths is not None:
+            if not path.endswith("/"):
+                path += '/'
+            for pat in excluded_paths:
+                if not pat.endswith("*"):
+                    pat += "*"
+                if fnmatch(path, pat):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorize header
+
+        Args:
+            request (_type_, optional):the request. Defaults to None.
+
+        Returns:
+            str: returns None
+        """
+        if request is not None:
+            return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns current user
+
+        Args:
+            request (_type_, optional):the request. Defaults to None.
+
+        Returns:
+            TypeVar: returns None
+        """
+        return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request
+        """
+        if request:
+            session_name = getenv("SESSION_NAME")
+            print()
+            return request.cookies.get(session_name)
+        return None
+
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,50 @@
+
+#!/usr/bin/env python3
+"""Creates Session Authentication
+"""
+from api.v1.auth.auth import Auth
+from uuid import uuid4
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session Auth
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Function to create user session
+        based on id
+        """
+        if user_id:
+            if isinstance(user_id, str):
+                id = str(uuid4())
+                self.user_id_by_session_id[id] = user_id
+                return id
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Get the User id based on the Session id
+        """
+        if session_id:
+            if isinstance(session_id, str):
+                return self.user_id_by_session_id.get(session_id, None)
+        return None
+
+    def current_user(self, request=None):
+        """Returns a User instance based on the request
+        """
+        session_cookies = self.session_cookie(request)
+        return User.get(self.user_id_for_session_id(session_cookies))
+
+    def destroy_session(self, request=None):
+        """Destroys an authenticated session.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if (request is None or session_id is None) or user_id is None:
+            return False
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
+        return True
+
