199688dd7993cd817255e17891932ca725ce555a
Jane <waguthijane61@gmail.com>
2024-07-17 06:02:16 -0700

    user_auth

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/env python3
+""" doc doc doc """
+from flask import Flask, jsonify, request
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """doc doc doc"""
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/", methods=["GET"])
+def welcome():
+    """doc doc doc"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,54 @@
+
+#!/usr/bin/env python3
+""" doc doc doc """
+
+from db import DB
+from user import User
+import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
+
+
+def _hash_password(password: str) -> bytes:
+    """doc doc doc"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """doc doc doc"""
+    return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """doc doc doc"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def register_user(self, email: str, password: str) -> User:
+        """doc doc doc"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
+
+    def create_session(self, email: str) -> str:
+        """doc doc doc"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
